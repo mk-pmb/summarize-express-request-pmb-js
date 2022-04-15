cab3fe7266cd64b911102ac9cdaf4e83184f5b19
mk-pmb <mk-pmb@users.noreply.github.com>
2022-04-15 17:55:56 +0200

    An example request. Some assembly required.

---
+++ test/fixtures/helloWorld.mjs
@@ -0,0 +1,98 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+import testUtil from '../testUtil.mjs';
+
+const baseUrl = '/hello/world';
+const qryStr = '?hello=world';
+const query = { hello: 'world' };
+
+const verbLineDict = {
+  baseUrl:,
+  method: 'GET',
+  originalUrl: baseUrl + qryStr,
+  url: '/' + qryStr, // NB: Express creates this slash from thin air.
+};
+
+const headerPairs = [
+  ['Host', 'localhost'],
+  ['User-Agent', 'Hello World Mock/1.1'],
+  ['Accept-Encoding', 'gzip, deflate'],
+  ['Connection', 'keep-alive'],
+];
+
+const hotDetails = {
+  headers: tu.pairs2obj_lckey(headerPairs),
+  params: { '0': 'test' },
+  query,
+};
+
+const advancedDetails = {
+  rawHeaders: headerPairs.flatten(),
+  rawTrailers: [],
+  statusCode: null,
+  statusMessage: null,
+};
+
+const lowlineDummies = {
+  _consuming: false,
+  _dumped: false,
+  _events: { mock: '_events' },
+  _eventsCount: 2,
+  _maxListeners: undefined,
+  _parsedUrl: { mock: '_parsedUrl' },
+  _readableState: { mock: '_readableState' },
+};
+
+const lowlineDescrs = {
+  _consuming: 'false',
+  _dumped: 'false',
+  _events: 'object',
+  _eventsCount: 'number',
+  _maxListeners: 'undefined',
+  _parsedUrl: 'object',
+  _readableState: 'object',
+};
+
+const boringDummies = {
+  aborted: false,
+  client: { mock: 'client' },
+  complete: false,
+  httpVersion: '1.1',
+  httpVersionMajor: 1,
+  httpVersionMinor: 1,
+  next: testUtil.bomb('next'),
+  res: { mock: 'res' },
+  socket: { mock: 'socket' },
+  upgrade: false,
+};
+
+const boringDescrs = {
+  aborted: 'false',
+  client: 'object',
+  complete: 'false',
+  httpVersion: 'string',
+  httpVersionMajor: 'number',
+  httpVersionMinor: 'number',
+  next: 'function',
+  res: 'object',
+  socket: 'object',
+  upgrade: 'false',
+};
+
+const detailedBase = {
+  '^': verbLineDict,
+  ...hotDetails,
+  ...advancedDetails,
+};
+
+
+const EX = {
+  boringDescrs,
+  boringDummies,
+  detailedBase,
+  hotDetails,
+  lowlineDescrs,
+  lowlineDummies,
+};
+
+export default EX;
